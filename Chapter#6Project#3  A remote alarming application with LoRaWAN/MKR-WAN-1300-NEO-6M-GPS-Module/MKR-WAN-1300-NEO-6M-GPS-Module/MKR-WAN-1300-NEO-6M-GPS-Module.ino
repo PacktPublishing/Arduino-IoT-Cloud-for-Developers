b1023997013bfd2bb4f6b17d79779d57366d83c0
Muhammad Afzal <5222380+mafzal9@users.noreply.github.com>
2023-08-31 10:21:33 +0500

    New Chapter#6 LoRaWAN
    New Chapter#6 LoRaWAN

---
+++ Chapter#6Project#3  A remote alarming application with LoRaWAN/MKR-WAN-1300-NEO-6M-GPS-Module/MKR-WAN-1300-NEO-6M-GPS-Module/MKR-WAN-1300-NEO-6M-GPS-Module.ino
@@ -0,0 +1,34 @@
+#include <TinyGPS++.h>
+
+TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
+void setup()
+{
+  
+  Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
+  Serial1.begin(9600);//This opens up communications to the GPS
+  Serial.println("GPS Start");//Just show to the monitor that the sketch has started
+}
+
+void loop()
+{x
+
+  while(Serial1.available()>0)//While there are characters to come from the GPS
+  {
+    gps.encode(Serial1.read());//This feeds the serial NMEA data into the library one char at a time
+  }
+  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+  {
+    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+    Serial.println("Satellite Count:");
+    Serial.println(gps.satellites.value());
+    Serial.println("Latitude:");
+    Serial.println(gps.location.lat(), 6);
+    Serial.println("Longitude:");
+    Serial.println(gps.location.lng(), 6);
+    Serial.println("Speed MPH:");
+    Serial.println(gps.speed.mph());
+    Serial.println("Altitude Feet:");
+    Serial.println(gps.altitude.feet());
+    Serial.println("");
+  }
+}
